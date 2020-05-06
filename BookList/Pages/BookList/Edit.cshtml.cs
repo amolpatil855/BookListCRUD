5bc617c9b1a31e11c7806b791db33dfd95b7ec10
purushottamrajdev <purushottam.rajdev@eternussolutions.com>
2020-05-06 16:29:39 +0530

    Performed Basic CRUD Operation

---
+++ BookList/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book =await _db.Book.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                var oldBook = await _db.Book.FindAsync(book.Id);
+                oldBook.Name = book.Name;
+                oldBook.Author = book.Author;
+                oldBook.ISBN = book.ISBN;
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+
+    }
+}
---
+++ BookList/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookList.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        //dependecy injection
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var bookToDelete = await _db.Book.FindAsync(id);
+            if (bookToDelete != null)
+            {
+                _db.Book.Remove(bookToDelete);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
